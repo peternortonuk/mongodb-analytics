fe3099c7020582e4c99971365cb99f9f5ba96d9b
pnorton <peter.norton@gazprom-mt.com>
2018-04-12 16:06:30 +0100

    blahh

--- intro-to-mongodb/notebooks/connect.py
+++ intro-to-mongodb/notebooks/connect.py
@@ -5,7 +5,7 @@ def uri(version='3.4'):
     raw_uri_dict = {'3.4': r'mongodb://analytics:<PASSWORD>@mflix-shard-00-00-heksn.mongodb.net:27017,mflix-shard-00-01-heksn.mongodb.net:27017,mflix-shard-00-02-heksn.mongodb.net:27017/test?ssl=true&replicaSet=mflix-shard-0&authSource=admin',
                     '3.6': r'mongodb+srv://analytics:<PASSWORD>@mflix-heksn.mongodb.net/test',
                     }
-    raw_uri = raw_uri_dict(version)
+    raw_uri = raw_uri_dict[version]
 
     # update the password
     password = r'analytics-password'
