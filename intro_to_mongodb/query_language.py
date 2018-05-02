2482d28b8930c4d26cc65b44b3311d8894a8d934
Peter Norton <peter.norton.uk@gmail.com>
2018-05-02 20:23:59 +0100

    more stuff

--- intro_to_mongodb/query_language.py
+++ intro_to_mongodb/query_language.py
@@ -2,6 +2,15 @@ from pymongo import MongoClient
 from notebooks.connect import uri
 import pprint
 
+'''
+=================================
+reference:
+https://docs.mongodb.com/manual/reference/method/db.collection.find/
+db.collection.find(query, projection)
+=================================
+'''
+
+
 # =================================
 # connect and define the collection
 
@@ -11,13 +20,25 @@ movies_collection = client.mflix.movies_initial
 # =================================
 # projection and filter
 
-projection = {}
-filter = {'language': 'Korean, English'}
+projection = {'title': 1, 'language': 1, '_id': 0}
+projection = None
+query = {'language': 'Korean, English'}
 
 
 # =================================
 # run the query
 
-results = movies_collection.find(projection, filter)
+results = movies_collection.find(query, projection).limit(3)
 pprint.pprint(list(results))
+
+
+# =================================
+# work with results cursor
+
+titles = []
+for row in results:
+    titles.append(row['title'])
+pprint.pprint(titles)
+
+
 
