e543a2561c4bd9af959ca1a5e08c675c05ac7165
peternortonuk <peter.norton.uk@gmail.com>
2018-05-04 16:48:11 +0100

    Update query_language.py

--- intro_to_mongodb/query_language.py
+++ intro_to_mongodb/query_language.py
@@ -1,4 +1,4 @@
-from pymongo import MongoClient
+from pymongo import MongoClient, DESCENDING
 from notebooks.connect import uri
 import pprint
 
@@ -15,20 +15,67 @@ db.collection.find(query, projection)
 # connect and define the collection
 
 client = MongoClient(uri('3.4'))
-movies_collection = client.mflix.movies_initial
+initial_collection = client.mflix.movies_initial
+clean_collection = client.mflix.movies_clean
 
 # =================================
 # projection and filter
+
+
+# =========================
+# movies_initial
 
 projection = {'title': 1, 'language': 1, '_id': 0}
 projection = None
 query = {'language': 'Korean, English'}
 
+# string must match exactly
+projection1 = {'_id': 0, 'language': 1}
+filter1 = {'language': 'Korean, English'}
+
+
+# =========================
+# movies_clean
+
+# must contain these values in exactly this order
+projection2 = {'_id': 0, 'languages': 1}
+filter2 = {'languages': ['Korean', 'English']}
+
+# must contain these values but without regard to order or other elements in the array
+filter3 = {'languages': {'$all': ['Korean', 'English']}}
+
+# must contain two elements but can be in any order
+# use implicit logical AND operator when using a comma separated list of expressions
+filter4 = {'languages': {
+    '$all': ['Korean', 'English'], '$size': 2,
+    }}
+
+# an explicit AND with the $and operator is necessary when the same field or operator has to be specified in multiple expressions.
+# doesnt work; i think this is a bug
+filter5 = {'&and': [
+    {'languages': {'$all': ['English', 'German']}},
+    {'languages': {'$size': 2}},
+    ]
+}
+# =================================
+# choose the collection, filter and projection
+
+filter = filter4
+projection = projection2
+collection = clean_collection
+sort_key = 'languages'
 
 # =================================
 # run the query
 
-results = movies_collection.find(query, projection).limit(3)
+results = collection.find(filter, projection).sort(sort_key, DESCENDING)
+length = 20
+results = results[:length]
+for result in results:
+    #print(type(result['language']))
+    pprint.pprint(result)
+
+results = collection.find(query, projection).limit(3)
 pprint.pprint(list(results))
 
 
@@ -39,6 +86,4 @@ titles = []
 for row in results:
     titles.append(row['title'])
 pprint.pprint(titles)
-
-
 
